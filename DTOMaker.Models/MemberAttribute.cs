cfe74f05372fa225bb8708d883dd45f45fb799b0
Psiman62 <psiman-hju@bytz.me>
2025-12-17 15:07:37 +1000

    imported MemBlocks attributes

--- DTOMaker.Models/MemberAttribute.cs
+++ DTOMaker.Models/MemberAttribute.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 
-namespace DTOMaker.Models
+namespace DTOMaker.Models;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class MemberAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class MemberAttribute : Attribute
+    public readonly int Sequence;
+
+    public MemberAttribute(int sequence)
     {
-        public readonly int Sequence;
-
-        public MemberAttribute(int sequence)
-        {
-            Sequence = sequence;
-        }
-
+        Sequence = sequence;
     }
+
 }
