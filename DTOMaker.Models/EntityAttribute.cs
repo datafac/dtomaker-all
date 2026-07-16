c2ba35ccbdedf7127861fbaff7abaf1c4b8a1bd5
Psiman62 <psiman-hju@bytz.me>
2026-07-16 21:44:09 +1000

    package updates

--- DTOMaker.Models/EntityAttribute.cs
+++ DTOMaker.Models/EntityAttribute.cs
@@ -5,6 +5,10 @@ namespace DTOMaker.Models
     [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
     public class EntityAttribute : Attribute
     {
+        /// <summary>
+        /// The unique identifier for the entity associated with the interface.
+        /// Todo - upgrade to GUIDs.
+        /// </summary>
         public readonly int EntityId;
 
         public EntityAttribute(int entityId)
