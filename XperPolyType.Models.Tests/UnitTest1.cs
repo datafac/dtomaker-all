f1a3dedfe5a46544d0953dcc2b32e71297f443e7
Psiman62 <psiman-hju@bytz.me>
2026-07-21 20:56:20 +1000

    xper with shapes

--- XperPolyType.Models.Tests/UnitTest1.cs
+++ XperPolyType.Models.Tests/UnitTest1.cs
@@ -1,27 +1,64 @@
 ﻿
 using PolyType.Examples.JsonSerializer;
 using PolyType.Examples.XmlSerializer;
+using PolyType.Examples.Cloner;
 using System.Text.Json.Serialization;
 using Xunit;
 using XperPolyType.Models;
+using PolyType;
+using System.Text;
+
+using VerifyXunit;
+using System.Threading.Tasks;
+using VerifyTests;
 
 namespace XperPolyType.Models.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        private string DumpShape<T>(T shape) where T : IShapeable<T>
         {
-            // todo try serializing Student
+            // do something with shape
+            ITypeShape<T> typeShape = T.GetTypeShape();
+            StringBuilder result = new StringBuilder();
+            result.AppendLineN($"Type   : {typeShape.Type.FullName}");
+            result.AppendLineN($"Kind   : {typeShape.Kind}");
+            result.AppendLineN($"Methods: {typeShape.Methods.Count}");
+            foreach (var method in typeShape.Methods)
+            {
+                result.AppendLineN($"       - {method.Name}");
+            }
+            result.AppendLineN($"Events : {typeShape.Events.Count}");
+            foreach (var evt in typeShape.Events)
+            {
+                result.AppendLineN($"       - {evt.Name}");
+            }
+            return result.ToString();
+        }
+
+        [Fact]
+        public async Task Test0()
+        {
             var student = new Student()
             {
                 FirstName = "John",
                 LastName = "Doe",
                 StudentId = "12345"
             };
+            await Verifier.Verify(student);
+        }
 
-            //string json = JsonSerializerTS.Serialize(student);
-            //string xml = XmlSerializer.Serialize(student);
+        [Fact]
+        public async Task Test1()
+        {
+            var student = new Student()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                StudentId = "12345"
+            };
+            string shape = DumpShape(student);
+            await Verifier.Verify(shape);
         }
     }
 }
---
+++ XperPolyType.Models/Partials.cs
@@ -0,0 +1,14 @@
+﻿using PolyType;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XperPolyType.Models
+{
+    public partial record Student
+    {
+        public void Something()
+        {
+        }
+    }
+}
