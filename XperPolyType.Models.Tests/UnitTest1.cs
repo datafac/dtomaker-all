1533042401d5e8e7d6280aeaf1bb9b9cf792f5ba
Psiman62 <psiman-hju@bytz.me>
2026-08-22 23:51:38 +1000

    cleanup

--- XperPolyType.Models.Tests/UnitTest1.cs
+++ XperPolyType.Models.Tests/UnitTest1.cs
@@ -16,29 +16,9 @@ namespace XperPolyType.Models.Tests
 {
     public class UnitTest1
     {
-        private ITypeShape<T> TypeShapeOf<T>(T shape) where T : IShapeable<T>
+        private ITypeShape<T> TypeShapeOf<T>() where T : IShapeable<T>
         {
             return T.GetTypeShape();
-        }
-
-        private string DumpShape<T>(T shape) where T : IShapeable<T>
-        {
-            // do something with shape
-            ITypeShape<T> typeShape = T.GetTypeShape();
-            StringBuilder result = new StringBuilder();
-            result.AppendLineN($"Type   : {typeShape.Type.FullName}");
-            result.AppendLineN($"Kind   : {typeShape.Kind}");
-            result.AppendLineN($"Methods: {typeShape.Methods.Count}");
-            foreach (var method in typeShape.Methods)
-            {
-                result.AppendLineN($"       - {method.Name}");
-            }
-            result.AppendLineN($"Events : {typeShape.Events.Count}");
-            foreach (var evt in typeShape.Events)
-            {
-                result.AppendLineN($"       - {evt.Name}");
-            }
-            return result.ToString();
         }
 
         [Fact]
@@ -60,19 +40,6 @@ namespace XperPolyType.Models.Tests
         }
 
         [Fact]
-        public async Task Test1()
-        {
-            var student = new Student()
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                StudentId = "12345"
-            };
-            string shape = DumpShape(student);
-            await Verifier.Verify(shape);
-        }
-
-        [Fact]
         public async Task Test2()
         {
             var student = new Student()
@@ -81,7 +48,7 @@ namespace XperPolyType.Models.Tests
                 LastName = "Doe",
                 StudentId = "12345"
             };
-            var typeShape = TypeShapeOf(student);
+            var typeShape = TypeShapeOf<Student>();
             await Verifier.Verify(typeShape);
         }
     }
